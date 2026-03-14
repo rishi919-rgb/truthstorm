8f346ca572378e6cd86ba59e7d2584d77f285b3e
Rishikesh Singh <rishikesh.singh.cg@gmail.com>
2026-03-14 15:55:33 +0530

    Create authentication API routes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -10,6 +11,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 // MongoDB Connection
 const connectDB = async () => {
